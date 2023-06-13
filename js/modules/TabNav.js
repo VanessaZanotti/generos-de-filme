2321806213769946f0a9da85ccacda1935a543fd
Vanessa Zanotti <zanottivanessa11@gmail.com>
2023-06-13 15:11:18 -0300

    refactor file tabnav

--- js/modules/TabNav.js
+++ js/modules/TabNav.js
@@ -1,22 +1,31 @@
-export default function iniTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
-  const activeClass = 'ativo';
-
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove(activeClass);
-    });
-    const direcao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add(activeClass, direcao);
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
   }
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add(activeClass);
 
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
-        activeTab(index);
-      });
+  // Ativa a tabe de acordo com o index da mesma
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass);
     });
+    const direcao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add(this.activeClass, direcao);
+  }
+
+  // Adiciona os eventos as tabs
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => this.activeTab(index));
+    });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      // Ativar primeiro item
+      this.activeTab(0);
+      this.addTabNavEvent();
+    }
   }
 }
