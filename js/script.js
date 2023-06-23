60b14cf4807a26116e5f22e8d136e44af9ab43e5
Vanessa Zanotti <zanottivanessa11@gmail.com>
2023-06-23 16:33:32 -0300

    ajustes

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ modal.init();
 const tooltip = new ToolTip('[data-tooltip]');
 tooltip.init();
 
-fetchFilmes('generos-de-filme/filmesapi.json', '.numeros-grid');
+fetchFilmes('/filmesapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
 
 const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
